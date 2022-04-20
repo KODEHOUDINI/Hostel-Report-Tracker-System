fdc7a0cbf82f6348b44880d614928d56cde593b1
KHEVLAR <lesley.edinam@gmail.com>
2022-04-20 10:47:38 +0000

    Final Version

--- index.js
+++ index.js
@@ -1,45 +1,75 @@
 const checkIn = document.forms[0];
 const checkOut = document.forms[1];
+const complaint = document.forms[2];
 const hist = document.querySelector(".hist");
+const currentDate = new Date();
 
-var checkhistory = [];
-var checkedIn = [];
-var checkedOut = [];
+// var checkHistory = JSON.parse(window.localStorage.getItem("history"));
+// var checkedIn = [];
+// var checkedOut = [];
+//declaring variabless
+
+if (JSON.parse(window.localStorage.getItem("history")) == null) {
+  var checkHistory = [];
+} else {
+  var checkHistory = JSON.parse(window.localStorage.getItem("history"));
+}
+
+if (window.localStorage.getItem("checkedIn") == null) {
+  var checkedIn = [];
+} else {
+  var checkedIn = JSON.parse(window.localStorage.getItem("checkedIn"));
+}
+
+if (window.localStorage.getItem("checkedOut") == null) {
+  var checkedOut = [];
+} else {
+  var checkedOut = JSON.parse(window.localStorage.getItem("checkedOut"));
+}
+
+updateHistory(checkHistory);
 
 checkIn.addEventListener("submit", (e) => {
-  try {
-    let day = new Date();
-    var newEntry = {
-      firstName: checkIn["fname"].value,
-      lastName: checkIn["lname"].value,
-      id: checkIn["id"].value,
-      phone: checkIn["phone"].value,
-      room: checkIn["room"].value,
-      date: day.toDateString(),
-      time: `${day.getHours()}:${day.getMinutes()}:${day.getSeconds()}`,
-      status: "Checked In",
-    };
-  } catch (err) {
-    alert("error " + err);
-  }
+  var newEntry = {
+    firstName: checkIn["fname"].value,
+    lastName: checkIn["lname"].value,
+    id: checkIn["id"].value,
+    phone: checkIn["phone"].value,
+    room: checkIn["room"].value,
+    date: getDate(),
+    time: getTime(),
+    status: "Checked In",
+  };
 
-  console.log(newEntry);
+  // console.log(newEntry);
 
-  //   checkedIn.forEach((record) => {
-  //     if (newEntry.id == record.id && newEntry.status === record.status ) {
-  //       // do popover to show person has already checked in
+  // if (checkedIn.length > 0) {
+  //   for (i = 0; i < checkedIn.length; i++) {
+  //     if (newEntry.id == checkedIn[i].id) {
+  //       alert("You have already checked in!");
   //     } else {
-  //       checkhistory.push(newEntry);
+  //       checkHistory.push(newEntry);
   //       checkedIn.push(newEntry);
-  //       //popover to notify successful checkin
+  //       window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  //       window.localStorage.setItem("checkedIn", JSON.stringify(checkedIn));
+  //       alert("You have successfully checked in 1");
   //     }
-  //   });
+  //   }
+  // } else {
+  //   checkHistory.push(newEntry);
+  //   checkedIn.push(newEntry);
+  //   window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  //   window.localStorage.setItem("checkedIn", JSON.stringify(checkedIn));
+  //   alert("You have successfully checked in 2");
+  // }
 
-  checkhistory.push(newEntry);
+  checkHistory.push(newEntry);
   checkedIn.push(newEntry);
+  window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  window.localStorage.setItem("checkedIn", JSON.stringify(checkedIn));
+  alert("You have successfully checked in!");
 
-  e.preventDefault();
-  updateHistory(checkhistory);
+  updateHistory(checkHistory);
 });
 
 checkOut.addEventListener("submit", () => {
@@ -49,27 +79,46 @@ checkOut.addEventListener("submit", () => {
     id: checkOut["id"].value,
     phone: checkOut["phone"].value,
     room: checkOut["room"].value,
-    date: "Monday",
-    time: "2:50pm",
+    date: getDate(),
+    time: getTime(),
     status: "Checked Out",
   };
 
-  checkedOut.forEach((record) => {
-    // if (newEntry.id === record.id && newEntry.status === record.status) {
-    //   // do popover to show person has already checked out
-    // } else {
-    //   history.push(newEntry);
-    //   checkedOut.push(newEntry);
-    //   //popover to notify successful checkout
-    // }
+  // if (checkedOut.length > 0) {
+  //   checkedOut.forEach((record) => {
+  //     if (newEntry.id === record.id && newEntry.day === record.day) {
+  //       // do popover to show person has already checked out
+  //       alert("You have already checked out!");
+  //     } else {
+  //       checkHistory.push(newEntry);
+  //       window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  //       checkedOut.push(newEntry);
+  //       window.localStorage.setItem("checkedOut", JSON.stringify(checkedOut));
+  //       alert("You have successfully checked out 1");
+  //     }
+  //   });
+  // } else {
+  //   checkHistory.push(newEntry);
+  //   window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  //   checkedOut.push(newEntry);
+  //   window.localStorage.setItem("checkedOut", JSON.stringify(checkedOut));
+  //   alert("You have successfully checked out 2");
+  // }
 
-    checkhistory.push(newEntry);
-    checkedOut.push(newEntry);
+  checkHistory.push(newEntry);
+  window.localStorage.setItem("history", JSON.stringify(checkHistory));
+  checkedOut.push(newEntry);
+  window.localStorage.setItem("checkedOut", JSON.stringify(checkedOut));
+  alert("You have successfully checked out!");
 
-    updateHistory(checkhistory);
-  });
+  updateHistory(checkHistory);
 });
 
+complaint.addEventListener("submit", () => {
+  alert("Your complaint has been sent!");
+});
+
+//fills the table with recent checkins/outs
 function updateHistory(history) {
   let showHistory = history.map((record) => {
     return `<tr>
@@ -87,3 +136,48 @@ function updateHistory(history) {
   showHistory = showHistory.join("");
   hist.innerHTML = showHistory;
 }
+
+//Gets current time
+
+function getTime() {
+  return currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+}
+
+// Gets current Date
+
+function getDate() {
+  return (
+    currentDate.getDate() + "/" + (currentDate.getMonth() + 1) + "/" + currentDate.getFullYear()
+  );
+}
+
+// unsused functions
+// function deleteEntry(entry, arr) {
+//   for (i = 0; i < arr.length; i++)
+//     if (entry.id == arr[i].id) {
+//       checkedOut.splice(i);
+//     }
+// }
+
+// function validateEntry(newEntry, arr, arrName, oppArray, message) {
+//   if (arr.length > 0) {
+//     for (i = 0; i < arr.length; i++) {
+//       if (newEntry.id == arr[i].id) {
+//         alert(`You have already ${message}`);
+//       } else {
+//         checkHistory.push(newEntry);
+//         checkedIn.push(newEntry);
+//         window.localStorage.setItem("history", JSON.stringify(checkHistory));
+//         window.localStorage.setItem(arrName, JSON.stringify(checkedIn));
+//         alert(`You have successfully ${message}1`);
+//         deleteEntry(newEntry, oppArray);
+//       }
+//     }
+//   } else {
+//     checkHistory.push(newEntry);
+//     checkedIn.push(newEntry);
+//     window.localStorage.setItem("history", JSON.stringify(checkHistory));
+//     window.localStorage.setItem("checkedIn", JSON.stringify(checkedIn));
+//     alert(`You have successfully ${message} 2`);
+//   }
+// }
